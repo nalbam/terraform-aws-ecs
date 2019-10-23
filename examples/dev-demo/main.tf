eb18e006bb208e6dcac1210e27f5bde61320a8b8
nalbam <me@nalbam.com>
2019-10-23 14:33:33 +0900

    examples

--- examples/dev-demo/main.tf
+++ examples/dev-demo/main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     region = "ap-northeast-2"
     bucket = "terraform-nalbam-seoul"
-    key    = "ecs-cluster.tfstate"
+    key    = "ecs-demo.tfstate"
   }
   required_version = ">= 0.12"
 }
